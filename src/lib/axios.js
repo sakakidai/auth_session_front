55cfed08686d2b3a1da2896b0e14935dd03ec201
sakaki <sakaki@samubuc.co.jp>
2022-06-27 00:20:33 +0900

    axiosにログ出力を追加

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,5 +1,19 @@
-import axios from 'axios'
+import _axios from 'axios'
 
-export default axios.create({
+const axios = _axios.create({
   baseURL: process.env.API_ORIGIN || 'http://localhost:3000',
 })
+
+//リクエスト インターセプター
+axios.interceptors.request.use((request) => {
+  console.log('Starting Request: ', request)
+  return request
+})
+
+//レスポンス インターセプター
+axios.interceptors.response.use((response) => {
+  console.log('Response: ', response)
+  return response
+})
+
+export default axios
