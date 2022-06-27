406bdab7f161002cae2ee4f17e8e850561849a5d
sakaki <sakaki@samubuc.co.jp>
2022-06-28 00:29:17 +0900

    APIの環境変数名の変更

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,7 @@
 import _axios from 'axios'
 
 const axios = _axios.create({
-  baseURL: process.env.API_ORIGIN || 'http://localhost:3000',
+  baseURL: process.env.API_URL || 'http://localhost:3000',
 })
 
 //リクエスト インターセプター
